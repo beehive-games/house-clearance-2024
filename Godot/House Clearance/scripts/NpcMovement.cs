9e599a1fdbaf6e5f15fc5320c52612ef86bae8ae
lordzargon <jr.maty@gmail.com>
2024-05-10 15:07:04 +0100

    initial NPC framework for movement - just mill around atm. Need to figure out how to ensure they dont try and get a target position through a wall or over a cliff edge

--- Godot/House Clearance/scripts/NpcMovement.cs
+++ Godot/House Clearance/scripts/NpcMovement.cs
@@ -10,6 +10,7 @@ public partial class NpcMovement : CharacterBody2D
 	[Export] private float _aggroDistance = 200.0f;	// At what distance, with line of sight to the player, will the NPC pursue player
 	[Export] private float _pursueDistance = 200.0f;	// When spotted, how far will NPC pursue player before returning to starting point
 	[Export] private float _patrolRadius = 200.0f;	// How far will NPC patrol from spawn location
+	[Export] private float _patrolWaitTime = 2.0f;	// How long can the NPC wait before finding a new target position?
 	[Export] private bool _canTeleport;					// Will the NPC go through teleport locations when pursuing
 	[Export] private float _health = 100f;
 	public AnimatedSprite2D SpriteNodePath;
@@ -19,6 +20,9 @@ public partial class NpcMovement : CharacterBody2D
 	public MoveState _moveState = MoveState.Idle;
 	public enum DeadState { Fall, Shot, HeadShot };
 	private DeadState _deadState = DeadState.Fall;
+	private Vector2 _startPosition;
+	private Vector2 _targetPosition;
+	private Timer _patrolWaitTimer;
 	
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -64,14 +68,72 @@ public partial class NpcMovement : CharacterBody2D
 		}
 	}
 
+	private void CancelWaitTimer()
+	{
+		_patrolWaitTimer?.Stop();
+		_patrolWaitTimer?.QueueFree();
+		_patrolWaitTimer = null;
+	}
+	
+	private void FinishedWaiting()
+	{
+		CancelWaitTimer();
+		_targetPosition = GetNewTargetPosition();
+	}
+
+	private void ReachedDestination()
+	{
+		CancelWaitTimer();
+		_patrolWaitTimer = new Timer();
+		float randomWait = new RandomNumberGenerator().RandfRange(0f,_patrolWaitTime);
+		_patrolWaitTimer.WaitTime = randomWait;
+		_patrolWaitTimer.OneShot = true;
+		_patrolWaitTimer.Autostart = true;
+		_patrolWaitTimer.Timeout += FinishedWaiting;	
+		var root = GetTree().Root;
+		root.AddChild(_patrolWaitTimer);
+	}
+
+	private Vector2 GetNewTargetPosition()
+	{
+		
+		
+		float randomPosOffset = new RandomNumberGenerator().RandfRange(-_patrolRadius,_patrolRadius);
+		Vector2 newPos = _startPosition;
+		newPos.X += randomPosOffset;
+		
+		var spaceState = GetWorld2D().DirectSpaceState;
+		var offsetGlobal = GlobalPosition;
+		offsetGlobal.Y += 10;
+		newPos.Y = GlobalPosition.Y - 3;
+		// Check for walls and door...
+		var rayQuery = PhysicsRayQueryParameters2D.Create(offsetGlobal, newPos);
+		var rayResult = spaceState.IntersectRay(rayQuery);
+		// Check for ground underneath
+		var pointQuery = new PhysicsPointQueryParameters2D();
+		pointQuery.Position = newPos;
+		
+		pointQuery.CollideWithAreas = true;
+		var pointResult = spaceState.IntersectPoint(pointQuery);
+		
+		Debug.WriteLine(rayResult);
+		Debug.WriteLine(pointResult);
+		
+		return newPos;
+	}
+	
 	public override void _Ready()
 	{
 		SpriteNodePath = GetNodeOrNull<AnimatedSprite2D>("AnimatedSprite2D");
+		_startPosition = GlobalPosition;
+		FinishedWaiting();
 	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+
+		
 		
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -84,20 +146,19 @@ public partial class NpcMovement : CharacterBody2D
 			MoveAndSlide();
 			return;
 		}
-		/*
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		
+		var dir = Mathf.Sign((_targetPosition-GlobalPosition).X);
+		var positionDelta = Mathf.Abs(GlobalPosition.X - _targetPosition.X);
+		bool atDestination = positionDelta < 2;
+		if (atDestination && _patrolWaitTimer == null )
 		{
-			velocity.X = direction.X * _speed;
+			ReachedDestination();
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
+			velocity.X = _speed * dir;
 		}
-		*/
-		velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
 		
 		Velocity = velocity;
 		MoveAndSlide();
